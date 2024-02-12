00bb4fe6ad5687fe26c432737e862abcebc1b6b1
Otaiki1 <otaikisadiq2804@gmail.com>
2024-02-12 01:18:36 +0100

    modify code

--- block_constructor.py
+++ block_constructor.py
@@ -1,50 +1,49 @@
 import csv
 
-class Pool:
-    def __init__(self, tx_id, fee, weight, parent_txids=None):
-        self.tx_id = tx_id
-        self.fee = fee
-        self.weight = weight
-        self.parent_txids = parent_txids
-
-def main():
-    MEMPOOL_FILE_PATH = "./mempool.csv"
-    BLOCK_SAMPLE_PATH = "./block_sample.txt"
-
-    pool_transactions = []
-
-    with open(MEMPOOL_FILE_PATH, newline='') as csvfile:
+def read_mempool_csv(file_path):
+    mempool = {}
+    with open(file_path, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            if len(row) > 3:
-                parent_ids = [p for p in row[3].split(";") if p]
-                structured = Pool(row[0], int(row[1]), int(row[2]), parent_ids)
-                pool_transactions.append(structured)
-            else:
-                structured = Pool(row[0], int(row[1]), int(row[2]))
-                pool_transactions.append(structured)
+            txid, fee, weight, parent_txids = row
+            fee = int(fee)
+            weight = int(weight)
+            parents = parent_txids.split(";") if parent_txids else []
+            mempool[txid] = {'fee': fee, 'weight': weight, 'parents': parents}
+    return mempool
 
-    arranged_transactions = arrange_transactions(pool_transactions)
-    remove_duplicate_transactions(arranged_transactions)
+def construct_block(mempool):
+    block_weight_limit = 4000000
+    block_weight = 0
+    block_transactions = []
 
-    with open(BLOCK_SAMPLE_PATH, 'a') as block_sample:
-        for transaction in arranged_transactions:
-            block_sample.write(f"{transaction.tx_id}\n")
+    def can_include(txid):
+        if txid in block_transactions:
+            return False
+        for parent_txid in mempool[txid]['parents']:
+            if parent_txid not in block_transactions:
+                return False
+        return True
 
-def arrange_transactions(transactions):
-    arranged_transactions = []
-    for transaction in transactions:
-        if transaction.parent_txids:
-            for parent_tx in transaction.parent_txids:
-                find_parent = next((pred for pred in transactions if pred.tx_id == parent_tx), None)
-                if find_parent:
-                    arranged_transactions.append(find_parent)
-        else:
-            arranged_transactions.append(transaction)
-    return arranged_transactions
+    sorted_txids = sorted(mempool.keys(), key=lambda x: mempool[x]['fee'], reverse=True)
+    for txid in sorted_txids:
+        if block_weight + mempool[txid]['weight'] <= block_weight_limit and can_include(txid):
+            block_transactions.append(txid)
+            block_weight += mempool[txid]['weight']
 
-def remove_duplicate_transactions(transactions):
-    transactions[:] = [t for i, t in enumerate(transactions) if t not in transactions[:i]]
+    return block_transactions
+
+def write_block_transactions_to_file(block_transactions, file_path):
+    with open(file_path, 'w') as file:
+        for txid in block_transactions:
+            file.write(txid + '\n')
+
+def main():
+    mempool_file_path = "./mempool.csv"
+    mempool = read_mempool_csv(mempool_file_path)
+    block_transactions = construct_block(mempool)
+    output_file_path = "./block_sample.txt"
+    write_block_transactions_to_file(block_transactions, output_file_path)
 
 if __name__ == "__main__":
     main()
